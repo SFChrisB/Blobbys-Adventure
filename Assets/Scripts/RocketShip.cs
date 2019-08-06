0972fafe66b4bc5e84c006ad7135158000f49901
Chris Bredl <85chrisjo@gmail.com>
2019-08-06 18:44:25 +0100

    42. Added RocketShip script and test rotation code

---
+++ Assets/Scripts/RocketShip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketShip : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)){
+            print("Space pressed");
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            print("Rotating left");
+        } else if (Input.GetKey(KeyCode.D)){
+            print("Rotating right");
+        }
+    }
+}
