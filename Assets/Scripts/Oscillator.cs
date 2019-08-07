9bc376c49425e82438c380a109b469bfe7d4f188
Chris Bredl <85chrisjo@gmail.com>
2019-08-08 00:30:38 +0100

    reset changes

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    //public members
+    [SerializeField] Vector3 movementVector;
+    // todo remove from inspector later
+    [SerializeField][Range(0,1)] float movementFactor; // 0 for not moved, 1 for moved
+
+    //private members
+    Vector3 startingPos;    //must be stored for absolute movement
+
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
