b9c692f23a1fec525f54f4c454f3c9e55c1d7b68
Chris Bredl <85chrisjo@gmail.com>
2019-08-08 03:11:05 +0100

    61. Creating moving obstacle asset and code

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     //public members
-    [SerializeField] Vector3 movementVector;
-    // todo remove from inspector later
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
     [SerializeField][Range(0,1)] float movementFactor; // 0 for not moved, 1 for moved
+    [SerializeField] float period = 2f;
+
 
     //private members
     Vector3 startingPos;    //must be stored for absolute movement
@@ -20,6 +21,13 @@ public class Oscillator : MonoBehaviour
 
     void Update()
     {
+        //set movement factor
+        float cycles = Time.time / period;  //grows continuosly from 0
+
+        const float tau = Mathf.PI * 2;     //about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;    //0.5 needed to give us a value between 0 and 1
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
